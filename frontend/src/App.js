c64e692d8a5bb90a12e09542a5a13db3157b0e2a
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-06-13 09:59:02 +0530

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import ManageUserComponent from './pages/manage-user-component/manage-user-component'
+import {exact} from "prop-types";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Route path='/' component = {ManageUserComponent} exact/>
+      </Router>
+    </div>
+  )
+}
+
+export default App
